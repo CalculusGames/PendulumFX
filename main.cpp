0f1c50dcfa6e1bdc56b0d5369ae829ba657b4877
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-11-28 01:06:12 -0600

    fix: include cmath, define pi if not defined

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@
 #include <thread>
 #include <vector>
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
 #include "pendulum.hpp"
 
 int main(int argc, char** argv) {
